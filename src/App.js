fc7b0254c56fbb7f14d303fbbf37937d4636b369
JAYAVEL T <tjayaveljayavel@gmail.com>
2021-05-12 16:14:21 +0530

    commit-changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { useState } from 'react';
-import {ChromePicker} from 'react-color';
-import './App.css';
 import Bgcolor from './components/bgcolor';
 
 function App() {
- // const [color,setColor] = useState('#fff')
   return (
-    // <div className="App" style={{backgroundColor:`${color}`}}>
-    //         <ChromePicker color={color} onChange={updatedcolor=>setColor(updatedcolor.hex)}/>
-    // </div>
-    <>
     <Bgcolor/>
-    </>
   );
 }
 
